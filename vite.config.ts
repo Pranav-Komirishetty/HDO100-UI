cf8a33e5494bdeffd95feddfaad6d0a236ac898f
Pranav <pranavkomirishetty@gmail.com>
2026-03-29 22:30:34 +0530

    PWA changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { VitePWA } from "vite-plugin-pwa"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "100 Days Challenge",
-        short_name: "100Days",
-        display: "standalone",
-        theme_color: "#6366f1"
-      }
-    }),
-    react()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_SHORT_NAME,
+          display: "standalone",
+          theme_color: "#6366f1"
+        }
+      }),
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
-
-
